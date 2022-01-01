7eb44c77b0b4585c61758fbad81e963a753e2695
GeorgeBodea <georgebodeacn@yahoo.com>
2022-01-01 18:47:10 +0200

    Change profile public route to private route
    Added a function for checking if the current user is logged in before entering a private route.
    
    If not, he will be redirected to the login  page.

--- src/components/AppRouters.jsx
+++ src/components/AppRouters.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 import { Home } from "../pages/HomePage" 
 import { Loginpage } from "../pages/LoginPage"
 import { Registerpage } from "../pages/RegisterPage"
 import { Profilepage } from "../pages/ProfilePage"
-import { Pagenotfound } from "../pages/PageNotFound" 
+import { Pagenotfound } from "../pages/PageNotFound"
+
+import { useAuth } from "../contexts/AppContexts"
+import { Navigate, Outlet } from 'react-router-dom'
 
 export default function AllRoutes(props) {
   return (
@@ -13,12 +16,25 @@ export default function AllRoutes(props) {
       <Router> 
           <Routes>
             <Route path="/" element={<Home/>}/>
+            <Route path="/home" element={<Home/>}/>
             <Route path="/login" element={<Loginpage/>}/>
             <Route path="/register" element={<Registerpage/>}/>
-            <Route path="/profile" element={<Profilepage/>}/>
+            <Route element={<RequireAuth />}>
+              <Route path="/profile" element={<Profilepage/>}/>
+            </Route>
             <Route path="*" element={<Pagenotfound/>}/>
           </Routes>
       </Router>
     </>
   )
+}
+
+function RequireAuth() {
+  let { currentUser } = useAuth();
+
+  if (!currentUser) {
+    return <Navigate to="/login" />;
+  }
+
+  return <Outlet />;
 }
